d17221d53d8f135b5bafcf8de64da46574731462
dodo <dodo@gmail.com>
2025-08-14 00:56:21 +0900

    Fast and Efficent finding odd

--- FindOdd/FindOdd/FindOdd.cpp
+++ FindOdd/FindOdd/FindOdd.cpp
@@ -8,15 +8,17 @@ CMyAPP theApp;
 CMyAPP theApp2;
 bool IsOdd(int number) 
 {
-	s_a = number / 2;
-	s_b = number - s_a;
+	//s_a = number / 2;
+	//s_b = number - s_a;
 
-	if (s_a == s_b)
-	{
-		return false; // Even number
-	}
-	else
-	{
-		return true;
-	}
+	//if (s_a == s_b)
+	//{
+	//	return false; // Even number
+	//}
+	//else
+	//{
+	//	return true;
+	//}
+
+	return ((number % 2) != 0); // Return true if number is odd, false if even
 }
